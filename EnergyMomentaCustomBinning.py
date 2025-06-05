ecf0aa7ced15e9d8f64212365606dcd1f3d175c5
IgalPress <press.igal@gmail.com>
2025-06-05 15:12:11 +0200

    Fixed the custom binning

--- EnergyMomentaCustomBinning.py
+++ EnergyMomentaCustomBinning.py
@@ -1,6 +1,7 @@
 import ROOT
 from ROOT import TCanvas
 from array import array
+import numpy as np
 
 latex = ROOT . TLatex ()
 latex . SetNDC ()
@@ -53,6 +54,7 @@ PionDMomentum = ROOT.RooRealVar("PionDMomentum", "", -1e8, 1e8)
 PionDSVDdEdx = ROOT.RooRealVar("PionDSVDdEdx", "", -1e8, 1e8)
 SlowPionMomentum = ROOT.RooRealVar("SlowPionMomentum", "", -1e8, 1e8)
 SlowPionSVDdEdx = ROOT.RooRealVar("SlowPionSVDdEdx", "", -1e8, 1e8)
+event_Dstar = ROOT.RooRealVar("event","",-1e8,1e8)
 
 ProtonMomentum = ROOT.RooRealVar("ProtonMomentum", "momentum for Proton (GeV)", -1e8, 1e8)
 ProtonSVDdEdxTrackMomentum = ROOT.RooRealVar("ProtonSVDdEdxTrackMomentum", "momentum for Proton (GeV), from the track", -1e8, 1e8)
@@ -60,6 +62,8 @@ ProtonSVDdEdx = ROOT.RooRealVar("ProtonSVDdEdx", "", -1e8, 1e8)
 PionLambdaMomentum = ROOT.RooRealVar("PionLambdaMomentum", "momentum for pion (GeV)", -1e8, 1e8)
 PionLambdaSVDdEdxTrackMomentum = ROOT.RooRealVar("PionLambdaSVDdEdxTrackMomentum", "momentum for pion (GeV), from the track", -1e8, 1e8)
 PionLambdaSVDdEdx = ROOT.RooRealVar("PionLambdaSVDdEdx", "", -1e8, 1e8)
+event_Lambda = ROOT.RooRealVar("event","",-1e8,1e8)
+
 
 variables_Dstar.add(KaonMomentum)
 variables_Dstar.add(PionDMomentum)
@@ -67,12 +71,14 @@ variables_Dstar.add(SlowPionMomentum)
 variables_Dstar.add(PionDSVDdEdx)
 variables_Dstar.add(SlowPionSVDdEdx)
 variables_Dstar.add(KaonSVDdEdx)
+variables_Dstar.add(event_Dstar)
 variables.add(ProtonMomentum)
 variables.add(ProtonSVDdEdxTrackMomentum)
 variables.add(ProtonSVDdEdx)
 variables.add(PionLambdaMomentum)
 variables.add(PionLambdaSVDdEdxTrackMomentum)
 variables.add(PionLambdaSVDdEdx)
+variables.add(event_Lambda)
 
 LambdaDataset = ROOT.RooDataSet("LambdaDataset", "LambdaDataset", preselTree, variables, "ProtonMomentum > 0.25")
 DstarDataset = ROOT.RooDataSet("DstarDataset", "DstarDataset", preselTreeDstar, variables_Dstar)
@@ -567,19 +573,50 @@ cSlowPionCustom.SaveAs("SlowPionCustomBinning.png")
 
 legend = ROOT.TLegend(0.7 ,0.6 ,0.85 ,0.75)
 
-hPionLambda_bg.Add(hPionD_bg)
-hPionLambda_bg.Add(hSlowPion_bg)
+treeDstar_sw.SetEstimate(treeDstar_sw.GetEntries() + 1)
 
-# Draw combined histogram
+treeDstar_sw.Draw("SlowPionMomentum/0.140*(event%2==0) + PionDMomentum/0.140*(event%2==1)", "", "goff")
+n_entries = treeDstar_sw.GetSelectedRows()
+vXP = treeDstar_sw.GetV1()
+
+xp_values = [vXP[i] for i in range(n_entries)]
+
+bins_input = array('d', xp_values)
+
+
+m_numPBins = 100
+kdBinsP = ROOT.TKDTreeBinning(n_entries, 1, bins_input, m_numPBins)
+binsMinEdgesP_orig = kdBinsP.SortOneDimBinEdges()
+
+binsMinEdgesP = [binsMinEdgesP_orig[i] for i in range(m_numPBins + 2)]
+binsMinEdgesP[0] = 0.1
+binsMinEdgesP[-1] = 25.0
+binsMinEdgesP_arr = array('d', binsMinEdgesP)
+
+m_numDEdxBins = 500
+m_dedxCutoff = 5e6
+
+hPionLambda_bg = ROOT.TH2F("hPionLambda_bg", "PionLambda", m_numPBins, binsMinEdgesP_arr, m_numDEdxBins, 0, m_dedxCutoff)
+hPionD_bg = ROOT.TH2F("hPionD_bg", "PionD", m_numPBins, binsMinEdgesP_arr, m_numDEdxBins, 0, m_dedxCutoff)
+hSlowPion_bg = ROOT.TH2F("hSlowPion_bg", "SlowPion", m_numPBins, binsMinEdgesP_arr, m_numDEdxBins, 0, m_dedxCutoff)
+
+treeLambda_sw.Draw("PionLambdaSVDdEdx:PionLambdaMomentum/0.1396>>hPionLambda_bg", "nSignalLambda_sw*(PionLambdaSVDdEdx>0)", "goff")
+treeDstar_sw.Draw("PionDSVDdEdx:PionDMomentum/0.1396>>hPionD_bg", "nSignalDstar_sw*(PionDSVDdEdx>0)", "goff")
+treeDstar_sw.Draw("SlowPionSVDdEdx:SlowPionMomentum/0.1396>>hSlowPion_bg", "nSignalDstar_sw*(SlowPionSVDdEdx>0)", "goff")
+
+hTotal = hPionLambda_bg.Clone("hTotal")
+hTotal.Add(hPionD_bg)
+hTotal.Add(hSlowPion_bg)
+
+
 cCombined = ROOT.TCanvas("cCombined", "Combined 2D Histogram", 800, 600)
 cCombined.SetRightMargin(0.15)
-hPionLambda_bg.SetMinimum(0.)
+hTotal.SetMinimum(0.)
 cCombined.SetLeftMargin(0.12) 
-hPionLambda_bg.SetStats(False)
-hPionLambda_bg.SetTitle("Combined Energy vs Momentum Histogram")
-hPionLambda_bg.Draw("COLZ")
+hTotal.SetStats(False)
+hTotal.SetTitle("Combined Energy vs Momentum Histogram")
+hTotal.Draw("COLZ")
 cCombined.SaveAs("Combined2DHistogram.png")
-
 
 
 cCombinedCustomProfileX = ROOT.TCanvas("cCombinedCustomProfileX", "Custom Binning Combined 2D ProfileX", 800, 600)
@@ -588,7 +625,7 @@ cCombinedCustomProfileX.SetRightMargin(0.15)
 cCombinedCustomProfileX.SetLeftMargin(0.12)
 cCombinedCustomProfileX.SetLogy(True)
 
-CustomprofileX_Combined = hPionLambda_bg.ProfileX("CustomprofileX_Combined")
+CustomprofileX_Combined = hTotal.ProfileX("CustomprofileX_Combined")
 CustomprofileX_Combined.SetLineColor(ROOT.kRed)
 CustomprofileX_Combined.SetLineWidth(2)
 CustomprofileX_Combined.SetTitle("Combined Pion ProfileX")
@@ -601,11 +638,11 @@ CustomprofileX_Combined.SetStats(False)
 CustomFitCombined = ROOT.TF1(
     "CustomFitCombined",
     "[0] + [1]/pow((x + [2]), [3]) + [4]*x",
-    0.25, 5
+    0.25, 24
 )
 CustomFitCombined.SetParameters(100, 5000, 0.01, 12, 10)  # [6] is the slope of the rising tail
 
-CustomprofileX_Combined.Fit(CustomFitCombined, "", "", 0.25, 5)
+CustomprofileX_Combined.Fit(CustomFitCombined, "", "", 0.25, 24)
 
 CustomFitCombined.SetLineColor(ROOT.kBlue)
 CustomFitCombined.SetLineWidth(2)
@@ -657,83 +694,12 @@ legend.AddEntry(CustomFitProton,"Fit Protons")
 legend.SetLineWidth(0)
 legend.Draw("Same")
 
-CustomFitCombined.GetYaxis().SetRangeUser(5e5, 7e7)
+CustomFitCombined.GetYaxis().SetRangeUser(1e6, 1e7)
 CustomFitCombined.SetTitle("Momentum vs Mean Energy Loss")
 CustomFitCombined.GetYaxis().SetTitle("Mean Energy Loss")
 CustomFitCombined.GetXaxis().SetTitle("P/M (GeV/C)")
 
 cProfileFormulae.SaveAs("CustomCombinedFormulae.png")
-# gaussFitProfileY = ROOT.TF1("gaussfitProfileY","gaus",100e3 ,1000e3)
 
-# profileY.Fit(gaussFitProfileY, "R 0")
-
-# meanProfileY = gaussFitProfileY.GetParameter(1)/1e3
-# meanProfileYerr = gaussFitProfileY.GetParError(1)/1e3
-# sigmaProfileY = gaussFitProfileY.GetParameter(2)/1e3
-# sigmaProfileYerr = gaussFitProfileY.GetParError(2)/1e3
-# TextHeight = 1
-# latex.DrawText(0.4, TextHeight-0.3, f" MeanProfileY = {meanProfileY:.1f} +/- {meanProfileYerr:.2f} e3")
-# latex.DrawText(0.4, TextHeight-0.35, f" SigmaProfileY = {sigmaProfileY:.1f} +/- {sigmaProfileYerr:.2f} e3")
-
-# gaussFitProfileY.SetLineColor(ROOT.kBlue)
-# gaussFitProfileY.SetLineWidth(2)
-# gaussFitProfileY.Draw("SAME")
-
-# c6.SaveAs("Combined2DHistogram_ProfileY.png")
-
-
-
-
-
-
-
-
-
-# print('Starting pion momentum plot')
-
-# c2 = ROOT.TCanvas()
-# c2.cd()
-# # c2.SetLogy(True)
-
-# # LambdaTree.Draw("HistogramDstarPionMomentum:HistogramDstarPionEnergy>>canvas")
-
-# HistogramDstarPionMomentum.SetLineColor(ROOT.kBlack)
-# HistogramLambdaPionMomentum.SetLineColor(ROOT.kRed)
-# HistogramDstarSlowPionMomentum.SetLineColor(ROOT.kBlue)
-
-# HistogramLambdaPionMomentum.Draw("Same")
-# HistogramDstarPionMomentum.Draw("Same")
-# HistogramDstarSlowPionMomentum.Draw("Same")
-
-# # legend.AddEntry(HistogramDstarPionMomentum, "Dstar")
-# # legend.AddEntry(HistogramLambdaPionMomentum,"Lambda")
-
-# # gaussFitLambdaPionMomentum = ROOT.TF1("gaussfitLambdaPionMomentum","gaus",0 ,1)
-# # gaussFitDstarPionMomentum = ROOT.TF1("gaussfitDstarPionMomentum","gaus",0.25 ,3.5)
-
-# # HistogramLambdaPionMomentum.Fit(gaussFitLambdaPionMomentum, "R 0")  
-# # HistogramDstarPionMomentum.Fit(gaussFitDstarPionMomentum, "R same 0") 
-
-# # meanLambdaPionMomentum = gaussFitLambdaPionMomentum . GetParameter (1)
-# # widthLambdaPionMomentum = gaussFitLambdaPionMomentum . GetParameter (0)
-
-# # meanDstarPionMomentum = gaussFitDstarPionMomentum.GetParameter(1)
-# # widthDstarPionMomentum = gaussFitDstarPionMomentum.GetParameter(0)
-
-# # TextHeight = 1
-# # latex . DrawText (0.45 ,TextHeight-0.3 , " MeanDstar = %.1f "%( meanDstarPionMomentum ))
-# # # latex . DrawText (0.45 ,TextHeight-0.35 , " WidthDstar = %.1f "%( widthDstarPionMomentum ))
-
-# # latex . DrawText (0.45 ,TextHeight-0.35 , " MeanLambda = %.1f "%( meanLambdaPionMomentum ))
-# # # latex . DrawText (0.45 ,TextHeight-0.45 , " WidthLambda = %.1f "%( widthLambdaPionMomentum ))
-
-# legend.AddEntry(HistogramDstarSlowPionMomentum, "D^{*+}->D^{0}\pi^{+}")
-# legend.AddEntry(HistogramDstarPionMomentum, "D^{+}->\pi^{+}K^{-}")
-# legend.AddEntry(HistogramLambdaPionMomentum, "\Lambda->P\pi^{-}")
-# legend.SetLineWidth(0)
-# legend.Draw("same")
-
-# HistogramLambdaPionMomentum.SetTitle("Momentum Comparison of pions;Momentum;Events")
-
-# c2.SaveAs("PionMomenta.png")
+########################################################################################
 
